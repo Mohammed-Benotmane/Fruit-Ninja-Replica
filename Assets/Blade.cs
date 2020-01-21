cff2164f83b8ee297fea328d959b8445d5e6b5a2
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-01-21 14:44:41 +0100

    add bladeTrail

---
+++ Assets/Blade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    bool isCutting = false;
+    Rigidbody2D rb;
+    Camera cam;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            StartCutting();
+        }else if(Input.GetMouseButtonUp(0)){
+            StopCutting();
+        }   
+
+        if(isCutting){
+            UpdateCut();
+        }    
+    }
+
+    void UpdateCut(){
+        rb.position = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void StartCutting(){
+        isCutting = true;
+    }
+
+    void StopCutting(){
+        isCutting = false;
+    }
+
+}
