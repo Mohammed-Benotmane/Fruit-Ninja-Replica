339c94ab204849d8c0ff49d7887aa5834c0fa7eb
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-01-25 14:26:30 +0100

    add restart button

---
+++ Assets/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
