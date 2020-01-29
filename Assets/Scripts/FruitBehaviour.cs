8314dd00399c3fa38cc04316d1285ad5ea1ae75d
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-01-29 14:04:21 +0100

    add spawning fruits

--- Assets/Scripts/FruitBehaviour.cs
+++ Assets/Scripts/FruitBehaviour.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class FruitBehaviour : MonoBehaviour
 {
     public GameObject fruitSliced;
+    Rigidbody2D rb;
+    public float startForce = 15f;
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
     void OnTriggerEnter2D(Collider2D col){
         if(col.tag == "Blade"){
             Vector3 direction = (col.transform.position - transform.position).normalized;
---
+++ Assets/Scripts/FruitSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject fruitPrefab;
+    public Transform[] spawnPoints;
+    public float minDelay = .1f;
+    public float maxDelay = 1f;
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+
+    IEnumerator SpawnFruits(){
+        while(true){
+            float delay = Random.Range(minDelay,maxDelay);
+            yield return new WaitForSeconds(1f);
+            int spawnIndex = Random.Range(0 , spawnPoints.Length);
+            Transform spawnPoint = spawnPoints[spawnIndex];
+            Instantiate(fruitPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    }
